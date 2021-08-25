8a2de3e6f2fa9538a5006f879747b57894b3e441
Vorobushek312 <Voronov_95@list.ru>
2021-08-25 18:11:46 +0600

    исправил константу

--- userfile.py
+++ userfile.py
@@ -13,17 +13,17 @@ say = """Домашнее задание.
 while True:
     option = utilic.chek_int_input(say, [1, 2, 3, 4, 5])
     if option == 1:
-        FILENAME = input('Введите назывние фаила. (Если такой фаил существует то он перезапишеться).: ')
-        FILENAME += '.txt'
+        filename = input('Введите назывние фаила. (Если такой фаил существует то он перезапишеться).: ')
+        filename += '.txt'
         info_text = input('Введите что хотите записать в фаил: ')
-        with open(FILENAME, 'w') as file:
+        with open(filename, 'w') as file:
             file.write(info_text + '\n')
         print('Фаил сохоанен!')
     elif option == 2:
         find_list = []
-        FILENAME = utilic.chek_name_file()
+        filename = utilic.chek_name_file()
         find_text = input('Введите что хотите найти в фаиле.')
-        with open(FILENAME, 'r') as file:
+        with open(filename, 'r') as file:
             for line in file:
                 if find_text in line:
                     find_list.append(line)
@@ -31,13 +31,13 @@ while True:
             for line in find_list:
                 print(line, end='')
     elif option == 3:
-        FILENAME = utilic.chek_name_file()
+        filename = utilic.chek_name_file()
         add_text = input("Введите текст: ")
-        with open(FILENAME, 'a')as file:
+        with open(filename, 'a')as file:
             file.write(add_text + '\n')
     elif option == 4:
-        FILENAME = utilic.chek_name_file()
-        with open(FILENAME, 'r') as file:
+        filename = utilic.chek_name_file()
+        with open(filename, 'r') as file:
             for line in file:
                 print(line, end="")
     elif option == 5:
